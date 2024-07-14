2074a1e52e0e7b0491d33341d0eb2e1d20f83c21
fuzzball <github@fuckin.rash.jp>
2024-07-14 19:47:50 +0900

    Perpetuate FAB counter

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = Storage.fabCounter;
 
   void _incrementCounter() {
     setState(() {
@@ -68,6 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      Storage.fabCounter = _counter;
     });
   }
 
--- lib/storage.dart
+++ lib/storage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+enum _Key {
+  fabCounter,
+  ;
+}
+
 class Storage {
   static late final SharedPreferences _prefs;
 
@@ -12,5 +17,12 @@ class Storage {
     debugPrint('[Storage][init] enter');
     _prefs = await SharedPreferences.getInstance();
     debugPrint('[Storage][init] exit: prefs[${_prefs.hashCode}]');
+  }
+
+  /// FAB counter.
+  static int get fabCounter => _prefs.getInt(_Key.fabCounter.name) ?? 0;
+
+  static set fabCounter(int count) {
+    _prefs.setInt(_Key.fabCounter.name, count);
   }
 }
